b949e8af5df79672c283454d3ee22cc320a5c4e3
REDKING_11 <99126547+REDKING-11@users.noreply.github.com>
2025-08-26 00:21:50 +0300

    Nice Colors and made the text work as wanted

--- Frontend/src/pages/TacTicalShooterPage.jsx
+++ Frontend/src/pages/TacTicalShooterPage.jsx
@@ -1,11 +1,11 @@
-//import '../css/TTShooter.css'
+import '../css/TTShooter.css'
 import Header from '../HeaderTTS'
 import Button from '@mui/material/Button'
 export default function TacTicalshooter() {
     return (
         <> 
         <Header></Header>
-            <div className="d-flex flex-column" style={{width: "1000px"}}>
+            <div className="d-flex flex-column justify-content-start" style={{width: "1800px"}}>
                 <div className="align-self-end">
                     <h1 className="text-center">TacTical Shooter</h1>
                     <p>blalblbalbla</p>  <b>Ahh Yes</b>  <p>blablablbalab</p>
@@ -13,7 +13,7 @@ export default function TacTicalshooter() {
                 </div>
                 <div className="align-self-start">
                     <h2>About The Game</h2>
-                    <p></p>
+                    <p>Test</p>
                 </div>
             </div>
         </>
