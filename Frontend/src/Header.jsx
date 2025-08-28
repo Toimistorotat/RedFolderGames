e086abdeca00562aeb9c3951f38099800cb3a72a
REDKING_11 <99126547+REDKING-11@users.noreply.github.com>
2025-08-28 14:13:41 +0300

    Jokin Pieni muutos

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header() {
     const navigate = useNavigate();
-    return <nav class="ms-2 navbar navbar-expand-lg navbar-dark bg-dark">
+    return <nav class="fixed-top ps-2 navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">RedFolderGames</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@ export default function Header() {
                 <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav">
                     <li class="nav-item active">
-                      <Button className='custom-nav2' onClick={()=>navigate("/")}><b className='custom-nav'>home</b></Button>
+                      <Button className='custom-nav2' onClick={()=>navigate("/")}><b className='custom-nav'>Home</b></Button>
                     </li>
                     <li class="nav-item">
                       <Button className='custom-nav2' onClick={()=>navigate("/TTS")}><b className='custom-nav'>TacticalSHooter</b></Button>
