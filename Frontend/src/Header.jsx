fff5b44a312c9925d73780728629736bcb36bf57
REDKING_11 <99126547+REDKING-11@users.noreply.github.com>
2026-01-24 14:08:18 +0200

    Fixed Some issues on the phone size

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -6,9 +6,6 @@ export default function Header() {
     const navigate = useNavigate();
     return <nav className="fixed-top ps-2 navbar navbar-expand-lg navbar-dark background glowborder">
             <a className="navbar-brand" href="#">RedFolderGames</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav">
                     <li className="nav-item active">
@@ -27,3 +24,7 @@ export default function Header() {
                 </div>
             </nav>
 }
+
+/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button> */
