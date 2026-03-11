fb0a91e9d2ad4d14933a957e503cc0d6fceb89d9
REDKING_11 <99126547+REDKING-11@users.noreply.github.com>
2026-03-11 14:23:17 +0200

    Feedback looks much better

--- Rewrite/RedFolderGames/src/components/FeedBack.jsx
+++ Rewrite/RedFolderGames/src/components/FeedBack.jsx
@@ -5,13 +5,36 @@ import { Button } from '@mui/material'
 export function Feedback({ addFeedback }) {
     const [name, setName] = useState('')
     const [message, setMessage] = useState('')
+    const [accepted, setAccepted] = useState(false)
+
+    if (!accepted) {
+        return (
+            <div className='font-mono p-6 rounded-xl shadow-[5px_5px_25px_rgba(255,255,255,0.25)] bg-white/5 w-full border border-green-500/30 mt-5 pb-5'>
+                <div className="text-xs text-zinc-400">
+                        Admin / Thank_you_in_advance.txt
+                    </div>
+                <div className="flex items-center justify-between px-4 py-2 border-b border-zinc-800"></div>
+                <p className="mb-2 !text-green-500">{'>'} Welcome to the feedback terminal</p>
+                <p className="!text-green-500">{'>'} Please leave thoughtful feedback.</p>
+                <p className="!text-green-500">{'>'} No slurs, spam, or disrespectful content.</p>
+                <p className="mb-6 !text-green-500">{'>'} Violations may be removed.</p>
+
+                <button
+                    onClick={() => setAccepted(true)}
+                    className="border border-green-500 !text-green-500 px-4 py-2 hover:bg-green-500 hover:text-black transition bg-white/3"
+                >
+                    {'>>'} Accept
+                </button>
+            </div>
+        )
+    }
 
     return (
         <div className="divForm">
             <div className='pt-1 rounded-xl shadow-[5px_5px_25px_rgba(255,255,255,0.25)] bg-white/5 w-full border-1 border-white/10 mt-5 pb-5'>
                 <div className="flex items-center justify-between px-4 py-2 border-b border-zinc-800">
                     <div className="text-xs text-zinc-400">
-                        Preview / Gimme Some Feedback please.txt
+                        Feedback / Gimme_Some_Feedback_please.txt
                     </div>
                     <div className="flex gap-2">
                         <div className='h-5 w-5 bg-green-500 rounded-full flex justify-center'>
@@ -26,7 +49,6 @@ export function Feedback({ addFeedback }) {
                     </div>
                 </div>
                 <form onSubmit={e => addFeedback(e, name, message)}>
-
                     <div className="flex ml-2">
                         <h2 className="!font-mono text-[20px] !whitespace-pre-line !text-green-500 animate-pulse">{'>>'} Name:</h2>
                         <input
@@ -64,11 +86,18 @@ export function FeedbackList({ feedbacks }) {
 
     return (
         <div>
-            <h2>Feedback</h2>
-            <ul>
-                {list.map(f => (
-                    <li key={f.id}>
-                        <strong>{f.name}</strong>: {f.message}
+            <h2 className="text-3xl">Feedback</h2>
+            <p className="text-ms !text-white">Terminal Look will be added later on</p>
+
+            <ul className="flex flex-col gap-3">
+                {list.map((f, i) => (
+                    <li
+                        key={f.id}
+                        className={`flex ${i % 2 === 0 ? "justify-start" : "justify-end"}`}
+                    >
+                        <div className="max-w-[60%] bg-white/10 text-green-400 font-mono px-4 py-2 rounded-lg">
+                            <strong>{f.name}</strong>: {f.message}
+                        </div>
                     </li>
                 ))}
             </ul>
